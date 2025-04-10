4cb22da1c359b4b79376ceab372e2322c3f9c8bd
Avinash <anjalirajesh1412089@gmail.com>
2025-04-10 19:14:56 +0530

    update files

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// let arr =[1,2,4,5,6]
-// for(let num=0;num<arr.length;num++){
-//     if(arr[num]-num==2){
-//         console.log(num+1)
-//         return;
-//     }
-// }
-// let arr = [2,7,11,15]
-// let target = 9;
-// let left = 0;
-// let right = arr.length-1;
-//  console.log(arr)
+let arr =[1,2,4,5,6]
+for(let num=0;num<arr.length;num++){
+    if(arr[num]-num==2){
+        console.log(num+1)
+        return;
+    }
+}
+let arr1 = [2,7,11,15]
+let target = 9;
+let left = 0;
+let right = arr1.length-1;
+ console.log(arr1)
 
 //  let nasu = [2,3,4,6,7]
 //  nasu  [2] = 9
@@ -17,31 +17,29 @@
 
   //Qus1. calcuteArea
 
-//  function calcuteArea(length,width) {
+ function calcuteArea(length,width) {
    
-//   let area = length*width;
-//   console.log("Area of rectangle",area)
-// }
-// calcuteArea(12,5)
-//Qus2. calcute Even No odd no 
-// function calcuteEvenNo(num) {
-//     if (num % 2 == 0) {
-//         console.log(num + " is even number");
-//         }
-//         else {
-//             console.log(num + " is odd number");
+  let area = length*width;
+  console.log("Area of rectangle",area)
+}
+calcuteArea(12,5)
+// Qus2. calcute Even No odd no 
+function calcuteEvenNo(num) {
+    if (num % 2 == 0) {
+        console.log(num + " is even number");
+        }
+        else {
+            console.log(num + " is odd number");
 
-//         }
-// }
+        }
+}
 
-//     calcuteEvenNo(10)
+    calcuteEvenNo(10)
 
 
 //Qus3 Calcutethhre smallest no
 
-let x = 6;  
-console.log(++x);
-console.log(x++);  
+ 
 
 
 
