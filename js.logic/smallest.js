ae78b6185ed812cea231e6e1585c36ec75d35e32
Avinash <avidhuriya8307@gmail.com>
2025-04-12 16:31:35 +0530

    Initial commit

--- js.logic/smallest.js
+++ js.logic/smallest.js
@@ -1,15 +1,17 @@
-// function threeSmallest(a,b,c){
-// if (a <= b && a <= c) {
-//     return a;
-//     } else if (b <= a && b <= c) {
-//         return b;
-//         } else {
-//             return c;
+function threeSmallest(a,b,c){
+if (a <= b && a <= c) {
+    return a;
+    } else if (b <= a && b <= c) {
+        return b;
+        } else {
+            return c;
 
-// }
+}
 
-// }
-// console.log(threeSmallest(3,3,3))
+}
+console.log(threeSmallest(3,3,3))
+
+
 function outer(){
     let name = "Roht";
     function inner(){
